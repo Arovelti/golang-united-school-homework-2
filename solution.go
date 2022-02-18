7d4f9fc8e8561a636f0a7aa51bf2b0fd15ab8fef
Dmitri <dmitri.arovelti@gmail.com>
2022-02-19 01:00:06 +0300

    Add const, Change SidesTriangle

--- solution.go
+++ solution.go
@@ -12,20 +12,20 @@ import "math"
 
 type customInt int
 
-var SidesTriangle customInt = 3
-var SidesSquare customInt = 4
-var SidesCircle customInt = 0
+const SidesTriangle = 3
+const SidesSquare = 4
+const SidesCircle = 0
 
 func CalcSquare(sideLen float64, sidesNum customInt) float64 {
 	switch sidesNum {
 	case SidesTriangle:
-		return math.Sqrt(3 * sideLen * sideLen)
+		return math.Sqrt(3) * sideLen * sideLen / 4
 	case SidesSquare:
 		return sideLen * sideLen
 	case SidesCircle:
 		return math.Pi * sideLen * sideLen
 	default:
-		return 0.0
+		return 0
 	}
 
 }
